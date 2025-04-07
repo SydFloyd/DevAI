0aa4f5cd1897c3072a3ffec8d8f1216bf3503b57
Kyle <kyleaddison98@gmail.com>
2025-04-06 21:05:54 -0400

    idk

---
+++ single_agent.py
@@ -0,0 +1,6 @@
+from src.interface.single_agent import SessionManager
+
+
+if __name__ == "__main__":
+    manager = SessionManager()
+    manager.start_session()
--- single_agent_example.py
+++
@@ -1,5 +0,0 @@
-from src.interface.single_agent import SessionManager
-
-manager = SessionManager()
-
-manager.start_session()
